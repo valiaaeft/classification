bf7a2e3ed1971ed8ea5886e1730d14a45d5c0ad7
valiaaeft <92579994+valiaaeft@users.noreply.github.com>
2022-08-05 16:40:29 +0300

    Create example1.py

---
+++ KNeighborsClassifier/example1.py
@@ -0,0 +1,13 @@
+import time
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+
+training = [[1, 1], [-5, 6]]
+labels  =[[1], [2]]
+neigh = KNeighborsClassifier(n_neighbors=1)  #initialise the classifier
+neigh.fit(training, labels)                      #fit the training set = conductors
+queries = [[30, -1], [-20, 3], [1, 4], [9, 100]]
+d =  neigh.predict(queries)
+
+print(d)
